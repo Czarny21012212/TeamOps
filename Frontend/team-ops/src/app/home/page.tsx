85d735f7c7f4b6d4f4e6dbafa9ebb81910b3b3d4
Wiktor <wiktorstachula20@gmail.com>
2025-08-13 11:30:04 +0200

    add funtion to show company with optimization

--- Frontend/team-ops/src/app/home/page.tsx
+++ Frontend/team-ops/src/app/home/page.tsx
@@ -1,21 +1,24 @@
 'use client';
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 export default function Home() {
 
     type UserData = {
         firstName?: string;
-        // add other fields if needed
+        
     };
 
+    type CompanyData = {
+        companyName?: string;
+        companyId?: number;
+    }
+
     const [userData, setUserData] = useState<UserData>({})
+    const [companyData, setCompanyData] = useState<CompanyData>({});
 
     useEffect(() => {
         fetch("http://localhost:8081/api/dataAboutUser", {
             method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            },
             credentials: "include"
         })
         .then(response => {
@@ -32,11 +35,34 @@ export default function Home() {
         });
         
     }, []);
-    console.log("User Data:", userData);
+
+    useEffect(() => {
+        fetch("http://localhost:8081/api/showCompany", {
+            method: "GET",
+            credentials: "include"
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then(data => {
+            setCompanyData(data);
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
+        });
+
+    }, [userData])
     return(
         <div>
             <div>
-                <h3>Hello {userData ? userData.firstName : "Guest"}</h3>
+                <h3 className="p-4 m-10 w-50">Hello {userData && userData.firstName ? userData.firstName : "Guest"}</h3>
+                <div className="bg-gray-100 p-4 m-10 w-50 hover:bg-gray-200 cursor-pointer">
+                    <h2>Company Name: {companyData ? companyData.companyName : "No Company"}</h2>
+                    <h2>Company ID: {companyData ? companyData.companyId : "No Company ID"}</h2>
+                </div>
             </div>
         </div>
     );
